b1ef938439d218e03d05196ddba83e2ad106e4a6
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-02-14 15:22:04 +0900

    add static

--- toy/app.js
+++ toy/app.js
@@ -4,12 +4,19 @@ const dotenv = require("dotenv")
 const user_router = require("./routes/user")
 dotenv.config()
 
+// npm install nodemon -> 자동 수정 가능.
+//npx nodemon app
 
+
+// key는 port value는 PORT인 값을 app에 넣은 것.
 app.set('port',8000)
 console.log(process.env.PORT)
 
 app.set('views', __dirname + '/views')
 app.set("view engine", 'ejs')
+
+// static file 이용. public이라는 파일. 외부에서도 접근 가능.
+app.use(express.static(__dirname + "/public"))
 
 app.get('/', (req, res) =>{
     res.render('index')
