dc678910dc54c1664f4560339677275fd5e2bb4a
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-02-17 15:19:59 +0900

    Feat: Connect MongoDB

--- toy/app.js
+++ toy/app.js
@@ -1,10 +1,15 @@
 const express = require("express")
 const app = express()
-const dotenv = require("dotenv")
+
 const user_router = require("./routes/user")
 const mongoose = require("mongoose")
 
+
+// npm install dotenv
+const dotenv = require("dotenv")
 dotenv.config()
+const MONGO_URI = process.env.MONGO_URI
+// console.log(MONGO_URI)
 
 // npm install nodemon -> 자동 수정 가능.
 //npx nodemon app
@@ -12,13 +17,14 @@ dotenv.config()
 
 // key는 port value는 PORT인 값을 app에 넣은 것.
 app.set('port',8000)
-console.log(process.env.PORT)
+// console.log(process.env.PORT)
 
 app.set('views', __dirname + '/views')
 app.set("view engine", 'ejs')
 
 // static file 이용. public이라는 파일. 외부에서도 접근 가능.
 app.use(express.static(__dirname + "/public"))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) =>{
     res.render('index', {menu: "home"})
@@ -26,6 +32,11 @@ app.get('/', (req, res) =>{
 
 
 app.use("/user", user_router)
+
+// 몽고db 연결
+mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("Successfully connected to mongodb"))
+    .catch(e => console.log(e))
 
 app.listen(app.get('port'), ()=>{
     console.log(app.get('port'), '번 포트에서 서버 연결 중....')
--- toy/routes/user.js
+++ toy/routes/user.js
@@ -1,19 +1,52 @@
 const express = require("express")
 const router = express()
 
+//모델 불러온느 곳
+const User = require("../models/user")
+
 
 router.get('/', (req, res) =>{
-    const userList = ["Jinsu", "Soomin", "Seongmin", "Hanwool", "Woojin", "Kyuha"]
-    res.render('user/index', { userList: userList, menu: "user" })
+    // 모델 접근
+    User.find((err, userList) => {
+        if(err){
+            return res.status(500).send({error: 'database failure'});
+        }
+        console.log("user", userList)
+        res.render('user/index', { userList: userList, menu: "user" })
+    })
+    // user.find({status: "박사 과정"}) 이런식으로 필터할 수 있음
+
 })
 
 router.get('/new', (req, res) =>{
     res.render('user/new', { menu: "user" } )
 })
 
+// create operation
+router.post('/create', (req, res)=>{
+    let user = new User();
+    // form으로 받은 것은 req.body안에 있음
+    user.name = req.body.userName
+    user.age = req.body.userAge
+    user.status = req.body.status
+
+    user.save((err) =>{
+        if (err) {
+            return res.status(500).send({error: 'database failure'});
+        }
+        res.redirect('/user')
+    })
+})
+
 router.get('/:name', (req, res) =>{
     let name = req.params.name
-    res.render('user/user', { userName: name, menu: "user" } )
+    //하나 find
+    User.findOne({name:name}, (err, user) => {
+        if(err){
+            return res.status(500).send({error: 'datavase failure'});
+        }
+        res.render(`user/user`, { user: user, menu: "user" })
+    })
 })
 
 module.exports = router
